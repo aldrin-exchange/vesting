4055433724805757f513d31c8bf3242cfcb247e6
_nboavida <nuno@aldrin.com>
2022-07-19 16:45:48 +0100

    add vesting model interface

--- programs/vesting-treasury/src/lib.rs
+++ programs/vesting-treasury/src/lib.rs
@@ -11,11 +11,4 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod vesting_treasury {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
---
+++ programs/vesting-treasury/src/models/vesting.rs
@@ -0,0 +1,17 @@
+use crate::prelude::*;
+
+#[derive(Default, Debug)]
+#[account]
+pub struct Vesting {
+    pub admin: Pubkey,
+}
+
+impl Vesting {
+    pub const VAULT_PREFIX: &'static [u8; 5] = b"vault";
+    pub const SIGNER_PDA_PREFIX: &'static [u8; 6] = b"signer";
+
+    pub fn space() -> usize {
+        //TODO: this is a placeholder for the timebeing
+        8 as usize
+    }
+}
