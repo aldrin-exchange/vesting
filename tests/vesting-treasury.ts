b3113b87f4e7b5b503e6b187d511973f039e10d8
_nboavida <nuno@aldrin.com>
2022-07-19 16:17:06 +0100

    init anchor

---
+++ tests/vesting-treasury.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { VestingTreasury } from "../target/types/vesting_treasury";
+
+describe("vesting-treasury", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.VestingTreasury as Program<VestingTreasury>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
