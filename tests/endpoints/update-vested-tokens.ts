cc9de35592496c71c5c83e0973bc9eac0445fdb6
_nboavida <nuno@aldrin.com>
2022-07-27 22:42:36 +0100

    fmt

--- tests/endpoints/update-vested-tokens.ts
+++ tests/endpoints/update-vested-tokens.ts
@@ -1,11 +1,11 @@
 import { Keypair, PublicKey } from "@solana/web3.js";
 import { expect } from "chai";
-import { createMint, createAccount} from "@solana/spl-token";
+import { createMint, createAccount } from "@solana/spl-token";
 import { errLogs, provider, payer, getErr } from "../helpers";
 import { Vesting } from "../vesting";
 
 export function test() {
-    describe("update_vested_tokens", () => {
+  describe("update_vested_tokens", () => {
     const adminKeypair = Keypair.generate();
     let vesteeWallet: PublicKey;
     let vesteeWalletNew: PublicKey;
@@ -21,7 +21,7 @@ export function test() {
         9
       );
     });
-    
+
     beforeEach("create vestee wallet", async () => {
       vesteeWallet = await createAccount(
         provider.connection,
@@ -32,13 +32,11 @@ export function test() {
     });
 
     beforeEach("create vesting account", async () => {
-        vesting = await Vesting.init(
-          {
-            adminKeypair,
-            vesteeWallet,
-            mint: vestingMint,
-          }
-        )
+      vesting = await Vesting.init({
+        adminKeypair,
+        vesteeWallet,
+        mint: vestingMint,
+      });
     });
 
     it("works", async () => {
@@ -49,21 +47,29 @@ export function test() {
       const vestingInfoAfter = await vesting.fetch();
 
       // Check that the cumulativeVestedAmount changes state
-      expect(vestingInfoBefore.cumulativeVestedAmount.amount.toNumber()).to.eq(0);
-      expect(vestingInfoAfter.cumulativeVestedAmount.amount.toNumber()).to.above(0);
+      expect(vestingInfoBefore.cumulativeVestedAmount.amount.toNumber()).to.eq(
+        0
+      );
+      expect(
+        vestingInfoAfter.cumulativeVestedAmount.amount.toNumber()
+      ).to.above(0);
 
       // Everything else remains with the same default values
-      expect(vestingInfoAfter.totalVestingAmount.amount.toNumber()).to.eq(10_000);
+      expect(vestingInfoAfter.totalVestingAmount.amount.toNumber()).to.eq(
+        10_000
+      );
       expect(vestingInfoAfter.startTs.time.toNumber()).to.eq(1577836801);
       expect(vestingInfoAfter.cliffPeriods.toNumber()).to.eq(12);
       expect(vestingInfoAfter.totalPeriods.toNumber()).to.eq(48);
-      expect(vestingInfoAfter.periodType).to.deep.eq({monthly : {}});
+      expect(vestingInfoAfter.periodType).to.deep.eq({ monthly: {} });
       expect(vestingInfoAfter.admin).to.deep.eq(adminKeypair.publicKey);
       expect(vestingInfoAfter.mint).to.deep.eq(vestingMint);
       expect(vestingInfoAfter.vault).to.deep.eq(await vesting.vestingVault());
-      expect(vestingInfoAfter.cumulativeWithdrawnAmount.amount.toNumber()).to.eq(0);
+      expect(
+        vestingInfoAfter.cumulativeWithdrawnAmount.amount.toNumber()
+      ).to.eq(0);
       expect(vestingInfoAfter.vestingVaultBalance.amount.toNumber()).to.eq(0);
-      expect(vestingInfoAfter.unfundedLiabilities.amount.toNumber()).to.eq(0); 
+      expect(vestingInfoAfter.unfundedLiabilities.amount.toNumber()).to.eq(0);
     });
   });
 }
