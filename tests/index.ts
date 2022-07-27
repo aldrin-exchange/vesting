8bc0520047fc408e08767a6f67d235d6c3f29f58
_nboavida <nuno@aldrin.com>
2022-07-27 11:18:43 +0100

    ts tests

--- tests/index.ts
+++ tests/index.ts
@@ -2,12 +2,14 @@ import chai from "chai";
 
 import * as createVestingSchedule from "./endpoints/create-vesting-schedule";
 import * as changeVestingWallet from "./endpoints/change-vestee-wallet";
+import * as updateVestedTokens from "./endpoints/update-vested-tokens";
 
 import { airdrop, provider } from "./helpers";
 
 describe("vesting-treasury", () => {
     createVestingSchedule.test();
     changeVestingWallet.test();
+    updateVestedTokens.test();
 
 
   before("airdrop SOL to provider wallet", async () => {
