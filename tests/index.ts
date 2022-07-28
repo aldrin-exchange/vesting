7584e714a137c99f3abeabd3bb975cbc22040848
_nboavida <nuno@aldrin.com>
2022-07-28 12:31:18 +0100

    add integration test interface

--- tests/index.ts
+++ tests/index.ts
@@ -3,6 +3,7 @@ import chai from "chai";
 import * as createVestingSchedule from "./endpoints/create-vesting-schedule";
 import * as changeVestingWallet from "./endpoints/change-vestee-wallet";
 import * as updateVestedTokens from "./endpoints/update-vested-tokens";
+import * as fundVestingVault from "./endpoints/fund-vesting-vault";
 
 import { airdrop, provider } from "./helpers";
 
@@ -10,6 +11,7 @@ describe("vesting-treasury", () => {
     createVestingSchedule.test();
     changeVestingWallet.test();
     updateVestedTokens.test();
+    fundVestingVault.test();
 
 
   before("airdrop SOL to provider wallet", async () => {
