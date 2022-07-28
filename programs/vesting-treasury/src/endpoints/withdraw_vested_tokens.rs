a9f28360265304935385a555a9b7f7b4c1c14a0a
_nboavida <nuno@aldrin.com>
2022-07-28 13:40:52 +0100

    endpoint interface

---
+++ programs/vesting-treasury/src/endpoints/withdraw_vested_tokens.rs
@@ -0,0 +1,54 @@
+//! TODO
+
+use crate::prelude::*;
+
+use anchor_spl::token::{self, Token, TokenAccount};
+
+#[derive(Accounts)]
+pub struct WithdrawVestedTokens<'info> {
+    #[account(mut)]
+    pub wallet_authority: Signer<'info>,
+    // #[account(mut)]
+    // pub vesting: Account<'info, Vesting>,
+    // #[account(
+    //     mut,
+    //     constraint = vesting_vault.key() == vesting.vault.key()
+    //     @ err::acc("Vault input does not match the vault in the vesting account")
+    // )]
+    // pub vesting_vault: Account<'info, TokenAccount>,
+    // // pub mint: Account<'info, Mint>,
+    // #[account(
+    //     mut,
+    //     constraint = funding_wallet.mint == vesting.mint.key()
+    //     @ err::acc("Funding wallet must be of correct mint")
+    // )]
+    // pub funding_wallet: Account<'info, TokenAccount>,
+    // pub token_program: Program<'info, Token>,
+}
+
+pub fn handle(ctx: Context<WithdrawVestedTokens>, withdraw_amount: TokenAmount) -> Result<()> {
+    let accs = ctx.accounts;
+
+    // token::transfer(
+    //     accs.as_transfer_funds_from_funding_wallet_to_vault_context(),
+    //     funding_amount.amount,
+    // )?;
+
+    // accs.vesting.vesting_vault_balance =
+    //     TokenAmount::new(accs.vesting.vesting_vault_balance.amount + funding_amount.amount);
+
+    Ok(())
+}
+// impl<'info> FundVestingVault<'info> {
+//     fn as_transfer_funds_from_funding_wallet_to_vault_context(
+//         &self,
+//     ) -> CpiContext<'_, '_, '_, 'info, token::Transfer<'info>> {
+//         let cpi_accounts = token::Transfer {
+//             from: self.funding_wallet.to_account_info(),
+//             to: self.vesting_vault.to_account_info(),
+//             authority: self.wallet_authority.to_account_info(),
+//         };
+//         let cpi_program = self.token_program.to_account_info();
+//         CpiContext::new(cpi_program, cpi_accounts)
+//     }
+// }
