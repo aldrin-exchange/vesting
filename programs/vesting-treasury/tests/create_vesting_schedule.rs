404dbee9e40f18a9afbdea60ead839b922a133bd
_nboavida <nuno@aldrin.com>
2022-08-01 16:19:02 +0100

    save state integration tests

--- programs/vesting-treasury/tests/create_vesting_schedule.rs
+++ programs/vesting-treasury/tests/create_vesting_schedule.rs
@@ -8,8 +8,57 @@ use anchortest::{
     spl::{self, MintExt, TokenAccountExt},
     stub,
 };
+use serial_test::serial;
 use solana_sdk::instruction::Instruction;
 use solana_sdk::sysvar::rent;
+use std::str;
+use std::sync::{Arc, Mutex};
+
+#[test]
+#[serial]
+fn monthly_vesting_with_cliff() -> Result<()> {
+    let mut test = Tester::default();
+    let og_state = test.clone();
+
+    let vesting_amount = TokenAmount::new(10_000);
+    let start_ts = TimeStamp::new(1577836801); // Jan 01 2020
+    let cliff_periods = 12;
+    let total_periods = 48;
+    let period_type = 2; // Monthly
+
+    assert!(test
+        .create_vesting(
+            vesting_amount,
+            start_ts,
+            cliff_periods,
+            total_periods,
+            period_type
+        )
+        .is_ok());
+
+    let vesting = Vesting::try_deserialize(&mut test.vesting.data.as_slice())?;
+    // assert_eq!(vesting.admin, test.admin.key);
+    // assert_eq!(vesting.vestee_wallet, test.vestee_wallet.key);
+    // assert_eq!(vesting.mint, test.mint.key);
+    // assert_eq!(vesting.vault, test.vesting_vault.key);
+    // assert_eq!(vesting.signer, test.vesting_signer.key); // TODO
+
+    // assert_eq!(vesting.total_vesting_amount, vesting_amount);
+    // assert_eq!(vesting.cumulative_vested_amount, TokenAmount::new(0));
+    // assert_eq!(vesting.cumulative_withdrawn_amount, TokenAmount::new(0));
+    // assert_eq!(vesting.vault_balance, TokenAmount::new(0));
+    // assert_eq!(vesting.unfunded_liability, TokenAmount::new(0));
+    // assert_eq!(vesting.start_ts, start_ts);
+    // assert_eq!(vesting.total_periods, total_periods);
+    // assert_eq!(vesting.cliff_periods, cliff_periods);
+    // assert_eq!(vesting.period_type, PeriodType::Monthly);
+
+    // no other changes should have happened
+    // test.vesting = og_state.vesting.clone();
+    // assert_eq!(test, og_state);
+
+    Ok(())
+}
 
 #[derive(Clone, Debug, PartialEq)]
 struct Tester {
@@ -37,12 +86,22 @@ impl Default for Tester {
             "vesting_signer",
             &[Vesting::SIGNER_PDA_PREFIX, vesting.key.as_ref()],
         );
-        let vesting_vault = AccountInfoWrapper::new()
-            .pack(spl::token_account::new(vesting_signer.key))
-            .owner(token::ID);
-        let mint = AccountInfoWrapper::new()
+        // TODO: for sure this should be metioning the seeds
+        // seeds = [Vesting::VAULT_PREFIX, vesting.key().as_ref()],
+        let mint = AccountInfoWrapper::with_key(Pubkey::new_unique())
+            .mutable()
             .pack(spl::mint::new(vesting_signer.key))
             .owner(token::ID);
+
+        let vesting_vault = AccountInfoWrapper::with_key(
+            Pubkey::find_program_address(
+                &[Vesting::VAULT_PREFIX, vesting.key.as_ref()],
+                &vesting_treasury::ID,
+            )
+            .0,
+        )
+        .pack(spl::token_account::new(vesting_signer.key).mint(mint.key))
+        .owner(token::ID);
 
         let vestee_wallet = AccountInfoWrapper::new()
             .pack(spl::token_account::new(admin.key))
@@ -50,6 +109,8 @@ impl Default for Tester {
         let token_program = AccountInfoWrapper::with_key(token::ID).program();
         let system_program = AccountInfoWrapper::with_key(system_program::ID).program();
         let rent = AccountInfoWrapper::with_key(rent::ID).program();
+
+        // println!("admin KEYYYYY::::: => {:?}", rent::ID);
 
         Self {
             admin,
@@ -74,8 +135,17 @@ impl Tester {
         total_periods: u64,
         period_type: u32,
     ) -> Result<()> {
-        self.set_syscalls();
-
+        self.set_syscalls(CpiValidatorState::CreateVesting {
+            admin: self.admin.key,
+            vesting: self.vesting.key,
+            next_cpi: CreateVault {
+                admin: self.admin.key,
+                vesting_vault: self.vesting_vault.key,
+                vesting_signer: self.vesting_signer.key,
+                vesting: self.vesting.key,
+                mint: self.mint.key,
+            },
+        });
         let mut ctx = self.context_wrapper();
         let mut accounts = ctx.accounts()?;
 
@@ -105,25 +175,59 @@ impl Tester {
             .acc(&mut self.rent)
     }
 
-    fn set_syscalls(&self) {
-        stub::Syscalls::new(CpiValidator(CpiValidatorState::CreateVesting {
-            admin: self.admin.key,
-            vesting: self.vesting.key,
-        }))
-        .set();
+    fn set_syscalls(&self, state: CpiValidatorState) -> stub::Syscalls<CpiValidator> {
+        let validator = CpiValidator(Arc::new(Mutex::new(state)));
+
+        let syscalls = stub::Syscalls::new(validator);
+        syscalls.clone().set();
+
+        syscalls
+        // stub::Syscalls::new(CpiValidator(CpiValidatorState::CreateVesting {
+        //     admin: self.admin.key,
+        //     vesting: self.vesting.key,
+        //     next_cpi: CreateVault {
+        //         admin: self.admin.key,
+        //         vesting_vault: self.vesting_vault.key,
+        //         vesting_signer: self.vesting_signer.key,
+        //     },
+        // }))
+        // .set();
     }
 }
 
-struct CpiValidator(CpiValidatorState);
+#[derive(Clone, Debug)]
+struct CpiValidator(Arc<Mutex<CpiValidatorState>>);
+
+#[derive(Debug, PartialEq, Eq)]
 enum CpiValidatorState {
-    CreateVesting { admin: Pubkey, vesting: Pubkey },
+    CreateVesting {
+        admin: Pubkey,
+        vesting: Pubkey,
+        next_cpi: CreateVault,
+    },
+    CreateVestingVault(CreateVault),
     Done,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct CreateVault {
+    admin: Pubkey,
+    vesting_vault: Pubkey,
+    vesting_signer: Pubkey,
+    vesting: Pubkey,
+    mint: Pubkey,
 }
 
 impl stub::ValidateCpis for CpiValidator {
     fn validate_next_instruction(&mut self, ix: &Instruction, accounts: &[AccountInfo]) {
-        match self.0 {
-            CpiValidatorState::CreateVesting { admin, vesting } => {
+        let mut state = self.0.lock().unwrap();
+
+        match *state {
+            CpiValidatorState::CreateVesting {
+                admin,
+                vesting,
+                ref next_cpi,
+            } => {
                 let rent = Rent::default().minimum_balance(Vesting::space());
                 let expected_ix = system_instruction::create_account(
                     &admin,
@@ -138,7 +242,60 @@ impl stub::ValidateCpis for CpiValidator {
                 let mut lamports = vesting.lamports.borrow_mut();
                 **lamports = rent;
 
-                self.0 = CpiValidatorState::Done;
+                *state = CpiValidatorState::CreateVestingVault(next_cpi.clone());
+            }
+            CpiValidatorState::CreateVestingVault(CreateVault {
+                admin,
+                vesting_vault,
+                vesting_signer, // TODO: maybr remove?
+                vesting,
+                mint,
+            }) => {
+                let rent = Rent::default().minimum_balance(Vesting::space());
+                // let expected_ix = token::spl_token::instruction::initialize_account(
+                //     &token::ID,
+                //     &Pubkey::find_program_address(
+                //         &[Vesting::VAULT_PREFIX, vesting.as_ref()],
+                //         &vesting_treasury::ID,
+                //     )
+                //     .0,
+                //     &mint,
+                //     &vesting_signer,
+                // )
+                // .unwrap();
+
+                let expected_ix = system_instruction::create_account(
+                    &admin,
+                    &Pubkey::find_program_address(
+                        &[Vesting::VAULT_PREFIX, vesting.as_ref()],
+                        &vesting_treasury::ID,
+                    )
+                    .0,
+                    rent,
+                    token::TokenAccount::LEN as u64,
+                    &token::ID,
+                );
+
+                // let expected_ix = system_instruction::create_account_with_seed(
+                //     &admin,
+                //     &vesting_vault,
+                //     &vesting,
+                //     &str::from_utf8(Vesting::VAULT_PREFIX).unwrap(),
+                //     rent,
+                //     token::TokenAccount::LEN as u64,
+                //     &token::ID,
+                // );
+
+                assert_eq!(&expected_ix, ix);
+
+                let vesting_vault = accounts
+                    .iter()
+                    .find(|acc| acc.key() == vesting_vault)
+                    .unwrap();
+                let mut lamports = vesting_vault.lamports.borrow_mut();
+                **lamports = rent;
+
+                *state = CpiValidatorState::Done;
             }
             CpiValidatorState::Done => {
                 panic!("No more instructions expected, got {:#?}", ix);
